3c5d1fd004a90bc0b7f00050d78606bc777e535f
KamilStepien <stepienkamil97@gmail.com>
2021-09-09 22:11:06 +0200

    Dodanie metody rejestracji

--- Testio/src/app/services/auth.service.ts
+++ Testio/src/app/services/auth.service.ts
@@ -3,45 +3,68 @@ import { Injectable } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoginModel } from '../models/auth/login.model';
+import { RegisterModel } from '../models/auth/register.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  private _isLogged:boolean;
+  private _isLogged:boolean ;
+  private _responseMessage:string;
 
-  constructor(private router: Router, private http: HttpClient, ) { }
+  constructor(private router: Router, private http: HttpClient, ) {
+    this._isLogged = false;
+    this._responseMessage = null;
+   }
+
+  isResponseMessage()
+  {
+    return this._responseMessage != null;
+  }
 
   isLogged()
   {
-    return this._isLogged
+    return this._isLogged;
   }
 
-  login(login: LoginModel):boolean
+  responseMessage()
   {
-    this.http.post("https://localhost:5001/api/auth/login", login)
+    setTimeout(()=> this._responseMessage = null,5000);
+    return  this._responseMessage;
+  }
+
+  login(user: LoginModel)
+  {
+    this.http.post("https://localhost:5001/api/auth/login", user)
     .subscribe(response => {
       const token = (<any> response).token;
       localStorage.setItem("jwt", token);
       this._isLogged = false;
       this.router.navigate(["/"]);
-      return true;
     },
     err =>
     {
       this._isLogged = true;
-
     })
-    return false;
+  }
+
+  register(user: RegisterModel)
+  {
+    this.http.post("https://localhost:5001/api/auth/register",  user)
+    .subscribe(respons => {
+      this._responseMessage = "Konto zostało utworzone poprawanie";
+      setTimeout(()=> this.router.navigate(['/login']),5000);
+    },
+    err =>
+    {
+      this._responseMessage = "Wystąpił błąd podczas tworzenia konta";
+    })
   }
 
   logOut()
   {
     localStorage.removeItem("jwt");
-    this.router.navigate(["/login"])
+    this.router.navigate(["/login"]);
   }
-
-
-
 }
