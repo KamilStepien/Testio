12d6b5a4fd731b9ca9c2c67dd799afe91d60039c
KamilStepien <stepienkamil97@gmail.com>
2021-09-09 22:14:58 +0200

    Dodanie routingu do formularza rejestracji

--- Testio/src/app/app-routing.module.ts
+++ Testio/src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './guard/auth.guard';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
   {path: '', component: HomeComponent, canActivate: [AuthGuard]}
 ];
 
