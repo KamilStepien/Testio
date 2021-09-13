9b5cbcc333d8497fdddbfb9fa424ed69cbb13ac7
KamilStepien <stepienkamil97@gmail.com>
2021-09-13 19:54:33 +0200

    Dodanie okan za pomocą którego możemy dodać zadanie

---
+++ Testio/src/app/task-module/dialog-add-task/dialog-add-task.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { TaskAddModel } from 'src/app/models/task/task.model';
+import { TaskService } from 'src/app/services/task.service';
+import {MatDialogRef} from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-dialog-add-task',
+  templateUrl: './dialog-add-task.component.html',
+  styleUrls: ['./dialog-add-task.component.scss']
+})
+export class DialogAddTaskComponent{
+
+  constructor(private fb:FormBuilder, private taskService:TaskService,  public dialogRef: MatDialogRef<DialogAddTaskComponent>,)
+  {
+
+  }
+
+  addTaskFrom = this.fb.group(
+    {
+      nameTask:['',[Validators.required,Validators.maxLength(50)]],
+      descriptionTask:['', [Validators.required,Validators.maxLength(200)]]
+    }
+  )
+
+  addTask()
+  {
+    let model:TaskAddModel =
+    {
+      name : this.addTaskFrom.value.nameTask,
+      description : this.addTaskFrom.value.descriptionTask
+    }
+    this.taskService.addTask(model);
+    this.dialogRef.close();
+  }
+}
