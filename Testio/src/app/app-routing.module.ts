7b9b71f773df03d3e8f08fe560fae658076aa704
KamilStepien <stepienkamil97@gmail.com>
2021-09-18 13:32:13 +0200

    Zmiany w routingu, przekierowanie do dashboardu, tasków i customers

--- Testio/src/app/app-routing.module.ts
+++ Testio/src/app/app-routing.module.ts
@@ -1,14 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomerComponent } from './customer/customer.component';
 import { AuthGuard } from './guard/auth.guard';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { RegisterComponent } from './register/register.component';
+import { TaskModuleComponent } from './task-module/task-module.component';
 
 const routes: Routes = [
-  {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent},
-  {path: '', component: HomeComponent, canActivate: [AuthGuard]}
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  {
+    path: 'dashboard',
+    component: HomeComponent,
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: 'task',
+        component: TaskModuleComponent,
+      },
+      {
+        path: 'customers',
+        component: CustomerComponent,
+      },
+    ],
+  },
+  {path: "**" , component: PageNotFoundComponent}
 ];
 
 @NgModule({
